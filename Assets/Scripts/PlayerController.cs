5a836409344392cafef075874ab7f1dbb488b199
sxprr <magalajawad@gmail.com>
2026-04-13 23:07:38 +0100

    Added ray that makes sure player always looks a moon in order to resist

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,34 +19,7 @@ public class PlayerController : MonoBehaviour
         // 2. Handle Input (Just the trigger)
         if (Input.GetKeyDown(KeyCode.E))
         {
-            // 1. Define the Ray
-            Ray ray = new Ray(transform.position,transform.forward);
-            RaycastHit hit;
-
-            // 2. Visual Debugging (Draws a line in the SCENE VIEW for 2 seconds)
-            // It will be Green if it hits, Red if it misses.
-
-            //getMask will get the the layer by name
-            int layerMask = LayerMask.GetMask("Celestial");
-
-            //additional parameter added to force ray to hit trigger
-            bool didHit = Physics.Raycast(ray, out hit, 300f, layerMask, QueryTriggerInteraction.Collide);
-            Debug.DrawRay(transform.position, transform.forward * 100f, didHit ? Color.green : Color.red, 2f);
-               
-            // 3. Logic Check
-            if (didHit)
-            {
-                Debug.Log("<color=green>SUCCESS:</color> Raycast punched through to the Celestial layer!");
-                if (hit.collider.CompareTag("Celestial"))
-                {
-                    Debug.Log("<color=cyan>Targeting:</color> Moon Hit!");
-                    GameEvents.TriggerMoonResist();
-                }
-                else
-                {
-                    Debug.Log($"<color=white>Targeting:</color> Hit {hit.collider.name}, but it's not the Moon.");
-                }
-            }
+            HandleRay();
             
         }
     }
@@ -66,6 +39,39 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
 
+    }
+
+    void HandleRay()
+    {
+
+        // 1. Define the Ray
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        // 2. Visual Debugging (Draws a line in the SCENE VIEW for 2 seconds)
+        // It will be Green if it hits, Red if it misses.
+
+        //getMask will get the the layer by name
+        int layerMask = LayerMask.GetMask("Celestial");
+
+        //additional parameter added to force ray to hit trigger
+        bool didHit = Physics.Raycast(ray, out hit, 300f, layerMask, QueryTriggerInteraction.Collide);
+        Debug.DrawRay(transform.position, transform.forward * 100f, didHit ? Color.green : Color.red, 2f);
+
+        // 3. Logic Check
+        if (didHit)
+        {
+            Debug.Log("<color=green>SUCCESS:</color> Raycast punched through to the Celestial layer!");
+            if (hit.collider.CompareTag("Celestial"))
+            {
+                Debug.Log("<color=cyan>Targeting:</color> Moon Hit!");
+                GameEvents.TriggerMoonResist();
+            }
+            else
+            {
+                Debug.Log($"<color=white>Targeting:</color> Hit {hit.collider.name}, but it's not the Moon.");
+            }
+        }
     }
 
 
